1746656a8389bec3af6fd08501d79d960857dfd2
joanp <jperarnau@cendrassos.net>
2022-03-24 12:24:37 +0100

    repetir

--- acti599.php
+++ acti599.php
@@ -21,7 +21,7 @@
 
 
 
-        echo "<p>Activitat P1.1</p>";
+        echo "<p>Activitat P1.1 </p>";
         echo date("Y");
         echo "<br>";
         echo "La hora actual es " . date("d-m-Y h:i:sa");
