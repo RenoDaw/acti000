4e28c28f0342c43db35d4ec85229e1094231725c
joanp <jperarnau@cendrassos.net>
2022-03-24 21:59:25 +0100

    lllevar archivos ejercicio 3

--- acti597.php
+++ acti597.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Activitat P1.3</title>
 </head>
 <body>
+    <p></p>
     
+    <h1>Activitat P1.2</h1>
+    <h2>Transparencias PHP in Action</h2>
+    <h5>nom del acti597.php</h5>
+    </br>
+    <?php
+    echo "<a href='/php/acti5671.php'>Entra a els arxius</a> ";
+
+    ?>
 </body>
 </html>
