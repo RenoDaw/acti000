6797a17923f74cae34324a10dcd0a4e0fcca66fc
joanp <jperarnau@cendrassos.net>
2022-03-26 10:16:06 +0100

    añadir archivo para ver repositorio

---
+++ acti595.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>acti595.php</title>
+</head>
+<body>
+<?php
+    echo "<h1>Títol</h1>";
+    echo "<h2>Subtítol</h2>";
+    echo "<h5>acti595.php</h5>";
+    phpinfo();
+    ?>
+</body>
+</html>
