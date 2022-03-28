2b4a8dbf4aac132f0d3332a2ec779af5553760ff
joanp <jperarnau@cendrassos.net>
2022-03-28 19:52:02 +0200

    final

--- acti595.php
+++ acti595.php
@@ -12,7 +12,7 @@
     echo "<h2>Subtítol</h2>";
     echo "<h5 >acti595.php</h5>";
     echo "<a href='./exe7/acti5951.html'>Provar el de las pizzas</a></br>";
-    echo "<a href='./exe7/acti5959.html'>formulario de post</a>";
+    echo "<a href='./exe7/acti5959.php'>formulario de post</a>";
     ?>
     
 </body>
